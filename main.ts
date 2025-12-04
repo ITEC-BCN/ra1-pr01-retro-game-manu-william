0847a46c02666a6eb05b830b0f930da2781678d1
Manuel Félix <manuel.felix.7e8@itb.cat>
2025-12-04 18:30:43 +0100

    Commit Personajes Animación + Portada

--- main.ts
+++ main.ts
@@ -112,65 +112,9 @@ function personaje_4 () {
     controller.moveSprite(mySprite, 100, 0)
     scene.cameraFollowSprite(mySprite)
 }
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (pantallaActual == "confirmarInicio") {
-        Seleccionar_mapas()
-    }
-})
 function Seleccionar_personajes () {
     pantallaActual = "seleccionarPersonaje"
-    itemsPersonajes = [miniMenu.createMenuItem("bart", img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . 3 3 3 3 3 3 3 3 3 3 
-        . 3 3 3 3 3 . . . . . . . . . 3 
-        3 . . . . . . . . . . . . . . 3 
-        . . . . . . . 3 . . . . . . . 3 
-        . . . . . . . 3 . . . . . . . 3 
-        . . . . . . . 3 . . . . . . 3 3 
-        . . . . . . . . 3 3 . . 3 3 . . 
-        . . . . . . . . . . 3 3 . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `), miniMenu.createMenuItem("santa", img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
-        . . . . . . . . . . . . . . 3 . 
-        . . . . . . . . 3 3 . . . . 3 . 
-        . . . . . . . . 3 . . . . . 3 . 
-        . . . . . . . . 3 3 3 3 . . 3 . 
-        . . . . . . . . . . . . 3 3 3 . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `), miniMenu.createMenuItem("perro", img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . 3 3 3 3 3 3 3 . . . . . . . . 
-        . . . . . . . . 3 3 3 3 . . . . 
-        . . . 3 . . . . . . . . 3 3 3 . 
-        . . . 3 . . . . . . . . . . 3 3 
-        . . . 3 3 . . . . . . . . . . 3 
-        . . . . 3 3 . . . . . . . . . 3 
-        . . . . . 3 3 3 . . . . . . . 3 
-        . . . . . . . . 3 3 3 3 3 3 3 . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `)]
+    itemsPersonajes = [miniMenu.createMenuItem("Bart", assets.image`Bart0`), miniMenu.createMenuItem("Santa", assets.image`Santa0`), miniMenu.createMenuItem("Finn", assets.image`Finn`)]
     menuPersonajes = miniMenu.createMenuFromArray(itemsPersonajes)
     menuPersonajes.setTitle("Personajes")
     menuPersonajes.setButtonEventsEnabled(true)
@@ -197,13 +141,6 @@ function onMapa_Cancelar (selection: string, selectedIndex: number) {
     menuMapas.close()
     Menu_principal()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (pantallaActual == "jugando") {
-        mySprite.vy = -200
-    } else if (pantallaActual == "confirmarInicio") {
-        iniciar_juego()
-    }
-})
 function Seleccionar_mapas () {
     pantallaActual = "seleccionarMapa"
     itemsMapas = [
@@ -322,6 +259,18 @@ function Seleccionar_mapas () {
         onMapa_Cancelar(selection, selectedIndex)
     })
 }
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (pantallaActual == "jugando") {
+        mySprite.vy = -200
+    } else if (pantallaActual == "confirmarInicio") {
+        iniciar_juego()
+    }
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (pantallaActual == "confirmarInicio") {
+        Seleccionar_mapas()
+    }
+})
 function iniciar_juego () {
     pantallaActual = "jugando"
     scene.setBackgroundColor(9)
@@ -521,24 +470,7 @@ function onPersonaje_Cancelar (selection: string, selectedIndex: number) {
 function Menu_principal () {
     scene.setBackgroundColor(15)
     pantallaActual = "menuPrincipal"
-    items = [miniMenu.createMenuItem("Personajes", img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . f f f . . . . . . . 
-        . . . . . . f f f . . . . . . . 
-        . . . . . . . f . . . . . . . . 
-        . . . . . . . f . . . . . . . . 
-        . . . . . f f f f . . . f . . . 
-        . . f f f f . f . f f f f . . . 
-        . . . . . . . f . . . . . . . . 
-        . . . . . . . f . . . . . . . . 
-        . . . . . . . f . . . . . . . . 
-        . . . . . . f f f f . . . . . . 
-        . . . . . f f . . f . . . . . . 
-        . . . . . f . . . f f . . . . . 
-        . . . . f f . . . . f f . . . . 
-        . . . . f . . . . . . f . . . . 
-        `), miniMenu.createMenuItem("Mapas", img`
+    items = [miniMenu.createMenuItem("Personajes", assets.image`personaje`), miniMenu.createMenuItem("Mapas", img`
         8 . . . . . . . . . . . . 8 8 . 
         8 8 8 8 8 . . . . . 8 . . 8 . . 
         . . . . . . . . . . 8 8 8 . . . 
@@ -704,113 +636,11 @@ function personaje_1 () {
     scene.cameraFollowSprite(mySprite)
 }
 function personaje_3 () {
-    mySprite = sprites.create(assets.image`pato`, SpriteKind.Player)
+    mySprite = sprites.create(assets.image`Finn`, SpriteKind.Player)
     animation.runImageAnimation(
     mySprite,
-    [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . b 5 5 b . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . b b b b b 5 5 5 5 5 5 5 b . . 
-        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-        . . b d 5 5 b 1 f f 5 4 4 c . . 
-        b b d b 5 5 5 d f b 4 4 4 4 b . 
-        b d d c d 5 5 b 5 4 4 4 4 4 4 b 
-        c d d d c c b 5 5 5 5 5 5 5 b . 
-        c b d d d d d 5 5 5 5 5 5 5 b . 
-        . c d d d d d d 5 5 5 5 5 d b . 
-        . . c b d d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . b 5 b . . . 
-        . . . . . . . . . b 5 b . . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . b b b b b 5 5 5 5 5 5 5 b . . 
-        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-        . . b d 5 5 b 1 f f 5 4 4 c . . 
-        b b d b 5 5 5 d f b 4 4 4 4 4 b 
-        b d d c d 5 5 b 5 4 4 4 4 4 b . 
-        c d d d c c b 5 5 5 5 5 5 5 b . 
-        c b d d d d d 5 5 5 5 5 5 5 b . 
-        . c d d d d d d 5 5 5 5 5 d b . 
-        . . c b d d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
-        `,img`
-        . . . . . . . . . . b 5 b . . . 
-        . . . . . . . . . b 5 b . . . . 
-        . . . . . . . . . b c . . . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . . . . b b 5 d 1 f 5 5 d f . . 
-        . . . . b 5 5 1 f f 5 d 4 c . . 
-        . . . . b 5 5 d f b d d 4 4 . . 
-        b d d d b b d 5 5 5 4 4 4 4 4 b 
-        b b d 5 5 5 b 5 5 4 4 4 4 4 b . 
-        b d c 5 5 5 5 d 5 5 5 5 5 b . . 
-        c d d c d 5 5 b 5 5 5 5 5 5 b . 
-        c b d d c c b 5 5 5 5 5 5 5 b . 
-        . c d d d d d d 5 5 5 5 5 d b . 
-        . . c b d d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
-        `,img`
-        . . . . . . . . . . b 5 b . . . 
-        . . . . . . . . . b 5 b . . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . . . . b b 5 d 1 f 5 d 4 c . . 
-        . . . . b 5 5 1 f f d d 4 4 4 b 
-        . . . . b 5 5 d f b 4 4 4 4 b . 
-        . . . b d 5 5 5 5 4 4 4 4 b . . 
-        . . b d d 5 5 5 5 5 5 5 5 b . . 
-        . b d d d d 5 5 5 5 5 5 5 5 b . 
-        b d d d b b b 5 5 5 5 5 5 5 b . 
-        c d d b 5 5 d c 5 5 5 5 5 5 b . 
-        c b b d 5 d c d 5 5 5 5 5 5 b . 
-        . b 5 5 b c d d 5 5 5 5 5 d b . 
-        b b c c c d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
-        `,img`
-        . . . . . . . . . . b 5 b . . . 
-        . . . . . . . . . b 5 b . . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . . . . b b 5 d 1 f 5 d 4 c . . 
-        . . . . b 5 5 1 f f d d 4 4 4 b 
-        . . . . b 5 5 d f b 4 4 4 4 b . 
-        . . . b d 5 5 5 5 4 4 4 4 b . . 
-        . b b d d d 5 5 5 5 5 5 5 b . . 
-        b d d d b b b 5 5 5 5 5 5 5 b . 
-        c d d b 5 5 d c 5 5 5 5 5 5 b . 
-        c b b d 5 d c d 5 5 5 5 5 5 b . 
-        c b 5 5 b c d d 5 5 5 5 5 5 b . 
-        b b c c c d d d 5 5 5 5 5 d b . 
-        . . . . c c d d d 5 5 5 b b . . 
-        . . . . . . c c c c c b b . . . 
-        `,img`
-        . . . . . . . . . . b 5 b . . . 
-        . . . . . . . . . b 5 b . . . . 
-        . . . . . . b b b b b b . . . . 
-        . . . . . b b 5 5 5 5 5 b . . . 
-        . . . . b b 5 d 1 f 5 5 d f . . 
-        . . . . b 5 5 1 f f 5 d 4 c . . 
-        . . . . b 5 5 d f b d d 4 4 . . 
-        . b b b d 5 5 5 5 5 4 4 4 4 4 b 
-        b d d d b b d 5 5 4 4 4 4 4 b . 
-        b b d 5 5 5 b 5 5 5 5 5 5 b . . 
-        c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
-        c b d c d 5 5 b 5 5 5 5 5 5 b . 
-        . c d d c c b d 5 5 5 5 5 d b . 
-        . . c b d d d d d 5 5 5 b b . . 
-        . . . c c c c c c c c b b . . . 
-        . . . . . . . . . . . . . . . . 
-        `],
-    500,
+    assets.animation`finnAni`,
+    200,
     true
     )
     mySprite.ay = 400
